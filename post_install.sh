9c7b8fe7d52174b856a7b6ca7074ef666d49233f
agaley <agaley@student.42lyon.fr>
2022-12-11 18:57:46 +0100

    Fix password requirements switch to pwquality, add requiretty

--- post_install.sh
+++ post_install.sh
@@ -6,7 +6,7 @@
 #    By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/05 20:14:57 by agaley            #+#    #+#              #
-#    Updated: 2022/12/11 13:25:30 by agaley           ###   ########lyon.fr    #
+#    Updated: 2022/12/11 18:54:26 by agaley           ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,7 @@ sed -i 's/^.*PermitRootLogin.*$/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i $'s/^PASS_MAX_DAYS.*$/PASS_MAX_DAYS\t30/' /etc/login.defs
 sed -i $'s/^PASS_MIN_DAYS.*$/PASS_MIN_DAYS\t2/' /etc/login.defs
 sed -i $'s/^PASS_WARN_AGE.*$/PASS_WARN_AGE\t7/' /etc/login.defs
-sed -i $'s/^.*pam_cracklib\.so.*$/password required\tpam_cracklib.so minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username enforce_for_root\npassword required\tpam_cracklib.so difok=7/' /etc/pam.d/common-password
+sed -i 's/^.*pam_pwquality.so.*$/password'"\t"'requisite'"\t"'pam_pwquality.so minlen=10 ucredit=-1 dcredit=-1 lcredit=0 ocredit=0 maxrepeat=3 reject_username enforce_for_root'"\n"'password'"\t"'requisite'"\t"'pam_pwquality.so difok=7/' /etc/pam.d/common-password
 
 mkdir /var/log/sudo
 echo 'Defaults	passwd_tries=3' >> /etc/sudoers.d/42
@@ -27,6 +27,7 @@ echo 'Defaults	badpass_message="Not this. You are on Qwerty (:"' >> /etc/sudoers
 echo 'Defaults	logfile="/var/log/sudo/audit"' >> /etc/sudoers.d/42
 echo 'Defaults	log_input, log_output' >> /etc/sudoers.d/42
 echo 'Defaults	iolog_dir="/var/log/sudo"' >> /etc/sudoers.d/42
+echo 'Defaults	requiretty' >> /etc/sudoers.d/42
 echo 'Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"' >> /etc/sudoers.d/42
 
 addgroup user42
