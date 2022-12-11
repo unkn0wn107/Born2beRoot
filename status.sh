80b57928746c2d7d3bf8cd6382cd874cad295575
agaley <agaley@student.42lyon.fr>
2022-12-11 21:51:08 +0100

    Fix status vcpus and open connexions

--- status.sh
+++ status.sh
@@ -6,7 +6,7 @@
 #    By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/06 22:27:29 by agaley            #+#    #+#              #
-#    Updated: 2022/12/11 06:03:16 by agaley           ###   ########lyon.fr    #
+#    Updated: 2022/12/11 21:47:35 by agaley           ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,14 +15,14 @@
 wall <<EOF
 #Architecture: $(uname -a)
 #CPU physical: $(lscpu | grep '^CPU(s)' | awk '{ print $2 }')
-#vCPU: $(cat /proc/cpuinfo | grep processor | wc -l)
+#vCPU: $(awk '/cpu cores/{s+=$4} END {print s}' /proc/cpuinfo)
 #Memory Usage: $(free -m | grep Mem | awk '{printf "%s/%sMB (%.2f%%)", $3,$2,$3*100/$2 }')
 #Disk Usage: $(df -h | grep ' /$' | awk '{printf "%d/%s (%s)", $3,$2,$5}')
 #CPU Load: $(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 #Last boot: $(who -b | awk '{print $3" "$4" "$5}')
 #LVM use: $(lsblk | grep lvm | awk '{if ($1) {print "yes";exit;} else {print "no"} }')
-#Connection TCP: $(netstat -an | grep ESTABLISHED |  wc -l)
-#User log: $(who | cut -d " " -f 1 | sort -u | wc -l)
+#Connection TCP: $(ss -t | grep ESTAB | wc -l) ESTABLISHED
+#User log: $(who | awk '{print $1}' | sort -u | wc -l)
 #Network: IP $(hostname -I | awk '{print $1}') ($(cat /sys/class/net/ens3/address))
 #Sudo: $(grep 'COMMAND' /var/log/sudo/audit | wc -l) cmd
 EOF
